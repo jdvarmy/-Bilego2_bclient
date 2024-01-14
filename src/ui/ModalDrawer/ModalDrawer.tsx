ed50088b546559babc8b63b7e443f589f5400805
sergei_antonov <jdvarmy@gmail.com>
2024-01-14 17:10:18 +0700

    feat(2024): npm update

--- src/ui/ModalDrawer/ModalDrawer.tsx
+++ src/ui/ModalDrawer/ModalDrawer.tsx
@@ -30,11 +30,11 @@ export const ModalDrawer = ({ children, show, onClose }: Props) => {
           <Transition.Child
             as={Fragment}
             enter='transform transition duration-300'
-            enterFrom='opacity-0 scale-95 -translate-x-full'
-            enterTo='opacity-100 scale-100 translate-x'
+            enterFrom='opacity-0 -translate-x-full'
+            enterTo='opacity-100 translate-x'
             leave='ease-in duration-200'
-            leaveFrom='opacity-100 scale-100 translate-x'
-            leaveTo='opacity-0 scale-95 -translate-x-full'
+            leaveFrom='opacity-100 translate-x'
+            leaveTo='opacity-0 -translate-x-full'
           >
             <div className='flex absolute top-0 left-0 bg-blue-800 h-[100vh] p-6 overflow-hidden drop-shadow-[0_0_10px_rgba(123,123,171,.7)] shadow-purple'>
               {children}
